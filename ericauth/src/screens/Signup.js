f73f5ab9a86bf1df321c5d549e09a08ab6ca3cec
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-06-02 05:29:59 +0900

    ff

--- ericauth/src/screens/Signup.js
+++ ericauth/src/screens/Signup.js
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  ImageBackground,
+  Button,
+  SafeAreaView,
+  Alert,
+} from "react-native";
+import * as config from "../config";
 import styled from "styled-components/native";
-import { Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import QRCode from "react-native-qrcode-svg";
 
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: ${({ theme }) => theme.background};
-`;
-
-const Signup = () => {
-  return <Text>Hello World!!</Text>;
+const Signup = ({ navigation }) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.navbar}>
+        <Text style={styles.navtext}> 인증서 목록 </Text>
+      </View>
+      <View style={styles.qrbox}></View>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  navbar: {
+    flex: 1,
+    backgroundColor: "#2F3A4D",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  qrbox: {
+    flex: 8,
+  },
+  navtext: {
+    color: "white",
+  },
+});
 
 export default Signup;
