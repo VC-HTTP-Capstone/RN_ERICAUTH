c7f3a010a5278bce471f6ac971be682e1af755a7
qsc7342 <qsc7342@hanyang.ac.kr>
2022-06-02 05:28:28 +0900

    add qrname list

--- ericauth/src/screens/Signup.js
+++ ericauth/src/screens/Signup.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  ImageBackground,
+  Button,
+  SafeAreaView,
+  Alert,
+} from "react-native";
+import * as config from "../config";
 import styled from "styled-components/native";
-import { Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import QRCode from "react-native-qrcode-svg";
 
 const Container = styled.View`
   flex: 1;
@@ -9,7 +24,40 @@ const Container = styled.View`
   background-color: ${({ theme }) => theme.background};
 `;
 
-const Signup = () => {
+const Signup = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  let qrNameList = [];
+  const getQrList = async () => {
+    console.log("Fetched");
+    AsyncStorage.getItem("email", (err, result) => {
+      setEmail(result);
+    });
+    console.log(email);
+    console.log("Fetched");
+    let url = config.Server_URL + "/api/qr";
+    let options = {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify({
+        email: email,
+      }),
+    };
+    let response = await fetch(url, options);
+    console.log(response);
+    let responseOK = response && response.ok;
+    if (responseOK) {
+      let data = await response.json();
+      console.log(data);
+    } else {
+    }
+  };
+  useEffect(() => {
+    getQrList();
+  });
   return <Text>Hello World!!</Text>;
 };
 
