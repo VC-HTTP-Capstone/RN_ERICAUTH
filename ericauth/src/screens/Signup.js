effe79a804caffbd9a3a60e762a99e6797acb4cf
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-06-02 06:22:29 +0900

    우리 프언가..?

--- ericauth/src/screens/Signup.js
+++ ericauth/src/screens/Signup.js
@@ -54,9 +54,36 @@ const Signup = ({ navigation }) => {
     } else {
     }
   };
+  const getQrData = async (qrName) => {
+    AsyncStorage.setItem("qrName", qrName, () => {
+      console.log("큐알네임 저장 : ", qrName);
+    });
+    let url = config.Server_URL + "/api/qr/info";
+    let options = {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify({
+        email: email,
+        qrName: qrName,
+      }),
+    };
+    let response = await fetch(url, options);
+    let responseOK = response && response.ok;
+    if (responseOK) {
+      let data = await response.json();
+      AsyncStorage.setItem("qrData", data.data, () => {
+        console.log("큐알네임 저장 : ", data.data);
+      });
+    } else {
+    }
+  };
   useEffect(() => {
     getQrList();
-  });
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.navbar}>
@@ -64,7 +91,24 @@ const Signup = ({ navigation }) => {
       </View>
       <View style={styles.qrbox}>
         {qrNameList.map((qr, index) => (
-          <Text>{qr}</Text>
+          <View style={styles.parent}>
+            <View style={styles.child1}>
+              <Image
+                style={styles.qrbox2}
+                source={require("../../assets/qr.png")}
+              />
+            </View>
+            <View style={styles.child2}>
+              <Text>이름 : {qr}</Text>
+              <Text>발급 날짜 : 22.06.08</Text>
+              <Text>만료 날짜 : 23.06.08</Text>
+            </View>
+            <View style={styles.child3}>
+              <TouchableOpacity style={styles.btnbox}>
+                <Text style={styles.btntext}>View</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         ))}
       </View>
     </SafeAreaView>
@@ -86,6 +130,44 @@ const styles = StyleSheet.create({
     flex: 8,
   },
   navtext: {
+    color: "white",
+  },
+  qrbox2: {
+    resizeMode: "contain",
+    width: "100%",
+    height: "100%",
+  },
+  parent: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  child1: {
+    flex: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: "3%",
+    marginRight: "3%",
+  },
+  child2: {
+    flex: 5,
+    justifyContent: "center",
+    marginLeft: "3%",
+  },
+  child3: {
+    flex: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: "3%",
+    marginRight: "3%",
+  },
+  btnbox: {
+    height: "40%",
+    width: "80%",
+    backgroundColor: "#2D9CDB",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btntext: {
     color: "white",
   },
 });
